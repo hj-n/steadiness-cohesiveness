333aec9e1a70e4f71112a5942a5e0c63bf4a8e6f
Hyeon Jeon <hj@hcil.snu.ac.kr>
2020-09-24 20:57:24 +0900

    circle brushing implemented

--- web/src/components/FimifView.js
+++ web/src/components/FimifView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import * as d3 from 'd3'
 import styled from 'styled-components'
 
@@ -9,15 +9,23 @@ function FimifView(props) {
     let jsonFileName = props.dataset + "_" + props.method + ".json";
     let data = require("../json/" + jsonFileName);
     const embeddedData = data.map((d, i) => {
-        if(d.emb.length == 2) d.emb.push(i);   // avoid double pushing (due to rendering issue)
+        if(d.emb.length === 2) d.emb.push(i);   // avoid double pushing (due to rendering issue)
         return d.emb;
     });
 
-    
+    // circle brusing
+    const [cbx, setCbx]     = useState(0);
+    const [cby, setCby]     = useState(0);
+    const [bx, setBx]       = useState(0);     // current brushing position (x)
+    const [by, setBy]       = useState(0);     // current brushing position (x)
+    const [cbidx, setCbidx] = useState(0);     // circle index
+    const [isCb, setIsCb]   = useState(false); // is brushing?
+
+    const cbrush = useRef();
+
 
     useEffect(() => { 
         // Executes when new data arrives
-        
         const width = props.width;
         const height = props.height;
         const margin = { hor: props.width / 20, ver: props.height / 20 };
@@ -37,12 +45,14 @@ function FimifView(props) {
         
         const radius = 3;
 
-        const svg = d3.select("#scatterplot")
+        // ANCHOR SVG width / height setting
+        const svg = d3.select("#scatterplot" + props.dataset + props.method)
                       .attr("width", width + margin.hor * 2)
                       .attr("height", height + margin.ver * 2)
                         .append("g")
                         .attr("transform", "translate(" + margin.hor + ", " + margin.ver + ")");
         
+        // ANCHOR Scatterplot rending & interaction
         svg.selectAll("circle")
            .data(embeddedData)
            .join(
@@ -57,23 +67,46 @@ function FimifView(props) {
            );
         
         svg.selectAll("circle")
-           .on("mouseover", function(){ d3.select(this).style("opacity", 1)  .attr("r", radius + 2); })
-           .on("mouseout" , function(){ d3.select(this).style("opacity", 0.3).attr("r", radius);     })
-           .on("click", () => {
+           .on("mouseover", function(){ d3.select(this).style("opacity", 1).attr("r", radius + 2); })
+           .on("mouseout" , function(){ d3.select(this).style("opacity", 0.3).attr("r", radius);   })
+           .call(d3.drag()
+                   .on("start", function() { 
+                       let circleData = d3.select(this).data()[0];
+                       setIsCb(true); 
+                       setCbidx(circleData[2]);
+                       setCbx(xScale(circleData[0])); setBx(xScale(circleData[0]));
+                       setCby(yScale(circleData[1])); setBy(yScale(circleData[1]));
+                    })
+                   .on("drag", (event) => {
+                       setBx(event.x);
+                       setBy(event.y);
+                   })
+                   .on("end", () => {
+                       cbrush.current.style("opacity", 0.3);
+                       setIsCb(false);
+                   })
+           
+           )
+        // ANCHOR brushing
+        cbrush.current = svg.append("circle").style("opacity", 0);
+    }, [])
 
-            
-           });
 
-
-
-
-    }, [props.dataset, props.method])
+    // For brushing
+    useEffect(() => {
+        if(isCb) {
+            cbrush.current.style("opacity", 0.5)
+                          .attr("cx", cbx)
+                          .attr("cy", cby)
+                          .attr("r", () => { return Math.sqrt(Math.pow((cbx - bx),2) + Math.pow((cby - by),2))} )
+        }
+    }, [by])
 
 
     return (
         <div style={{width: props.width * 1.1}}>
             <H5>{props.method.toUpperCase()} embedding result of {props.dataset.toUpperCase()} dataset</H5>
-            <svg id="scatterplot"></svg>
+            <svg id={"scatterplot" + props.dataset + props.method}></svg>
         </div>
     )
 }
