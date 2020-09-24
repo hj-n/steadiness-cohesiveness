1ce668dacd293ce9312b6ac038464abcade1889f
Hyeon Jeon <hj@hcil.snu.ac.kr>
2020-09-24 16:24:09 +0900

    react template for fimif added

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-import sphere_tsne from "./json/sphere_tsne.json" 
+
+const useStyles = makeStyles({
+  header : {
+    background: "#8ae1f2", 
+    padding: 10,
+    color: "black"
+    
+  }
+})
+
 
 function App() {
 
-  console.log(sphere_tsne)
-
+  const classes = useStyles();
 
   return (
     <div>
+      <AppBar className={classes.header}>
+        <Typography variant="h6">
+          FiMiF
+        </Typography>
+      </AppBar>
       
     </div>
   );
