1c8fc45d774c89c1aa83d9554361779bbeab6045
Hyeon Jeon <hj@hcil.snu.ac.kr>
2020-10-16 00:53:21 +0900

    missing / false group generation implemented

--- fimif/measure_v2/measure.py
+++ fimif/measure_v2/measure.py
@@ -1,5 +1,9 @@
 import numpy as np
 import random
+import json
+
+from sklearn.neighbors import KDTree
+from pyclustering.cluster.xmeans import xmeans
 
 
 class Fimif:
@@ -8,7 +12,7 @@ class Fimif:
                  raw,     # raw data
                  emb,     # emb data
                  iteration=1000, # iteration number
-                 k=4,     # for constructing knn graph
+                 k=6,     # for constructing knn graph
                  walk_num=500, # random walk number
                  max_cluster_num=20, # max cluster num for x-means clustering
                  beta=1 # beta for F_beta score calculation
@@ -37,20 +41,42 @@ class Fimif:
 
     def __measure(self):
         for i in range(self.iter):
-            random_cluster = __random_cluster_selection()
+            random_cluster = self.__random_cluster_selection(True)
+            clusters = self.__find_groups(random_cluster, True)
+            
 
-    def __random_cluster_selection(self):
-        seed = random.randint(0, N - 1)
+
+    def __random_cluster_selection(self, is_false):
+        seed = random.randint(0, self.N - 1)
         random_cluster = set()
         random_cluster.add(seed)
 
+        neighbors = self.emb_neighbors if is_false else self.raw_neighbors
+
         visiting = seed  # current visiting point
         for _ in range(self.walk_num):
-            visiting = self.raw_neighbors[visiting][random.randint(0, self.k - 1)]
+            visiting = neighbors[visiting][random.randint(0, self.k - 1)]
             random_cluster.add(visiting)
         
         return random_cluster
 
+    def __find_groups(self, random_cluster, is_false):
+        random_cluster_list = list(random_cluster)
+        xmean_instance = None
+        if is_false:
+            xmean_instance = xmeans(self.raw[random_cluster_list], kmax=self.max_cluster_num, tolerance=0.01)
+        else:
+            xmean_instance = xmeans(self.emb[random_cluster_list], kmax=self.max_cluster_num, tolerance=0.01)
+        
+        xmean_instance.process()
+        clusters_idx = xmean_instance.get_clusters()
+        clusters = []
+        for cluster in clusters_idx:
+            clusters.append([random_cluster_list[idx] for idx in cluster])
+
+        return clusters
+
+        
         
 
 
@@ -73,4 +99,11 @@ class Fimif:
 
     
 
-    
+file = open("./json/sphere_tsne.json", "r") 
+data = json.load(file)
+
+raw = np.array([datum["raw"] for datum in data])
+emb = np.array([datum["emb"] for datum in data])
+
+Fimif(raw, emb, iteration=1)
+
