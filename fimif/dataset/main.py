da0513726e169cb46bfc3e431bcddc71a8cf7ca9
Hyeon Jeon <hj@hcil.snu.ac.kr>
2020-10-31 16:56:05 +0900

    added mammoth_umap data from pair-code/understanding-umap

--- fimif/dataset/main.py
+++ fimif/dataset/main.py
@@ -9,7 +9,10 @@ import numpy as np
 import pandas as pd
 import csv
 
+import json
+
 PATH_TO_WEB = "../../web/src/json/"
 PATH_TO_MEASURE = "./../fimif_measure/json/"
 PATH = PATH_TO_MEASURE
 
+json_file = 
