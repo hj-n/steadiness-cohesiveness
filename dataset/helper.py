266fda79c5676dd3cbd79be668180179ecd7bf66
Hyeon Jeon <hj@hcil.snu.ac.kr>
2021-03-07 22:27:35 +0900

    removed deprecated dataset generator

--- dataset/helper.py
+++
@@ -1,5 +0,0 @@
-
-
-def print_time_spent(start_time, end_time, data_info):
-    print("Time spent for " + data_info[0].upper() + " dataset using " + data_info[1].upper() + ": ", end="")
-    print(end_time - start_time, "seconds")
